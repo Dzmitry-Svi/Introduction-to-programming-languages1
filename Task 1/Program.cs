8e18cb889a216b3f0dfdea67586a427bcc9c5dd1
Dzmitry-Svi <dima.svitich@yandex.ru>
2023-03-10 00:16:01 +0300

    Решил 1-ю задачу

---
+++ Task 1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите 1-е число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+  Console.WriteLine("Большее число:a, меньшее число:b");
+}
+if (a < b)
+{
+  Console.WriteLine("Большее число:b, меньшее число:a");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
