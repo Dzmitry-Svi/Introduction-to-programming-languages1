a47b3d76f3d91a1aec0e9b0531edfba021f14bf8
Dzmitry-Svi <dima.svitich@yandex.ru>
2023-03-10 18:56:25 +0300

    Решил 2-ю задачу

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -3,5 +3,11 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе чило b");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число c");
-int c = Convert.ToInt32(Console.ReaLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+System.Console.WriteLine("Максимальное из этих чисел = " + max);
+
+
 
